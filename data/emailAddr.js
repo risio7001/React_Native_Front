52e314400a0510798c388413f733a55f1f7033d9
sun <hama006@flroad.co.kr>
2021-11-08 17:54:23 +0900

    211108 'login 100% signup 50% signupTerms 80% googleSignIn 100%'

---
+++ data/emailAddr.js
@@ -0,0 +1,6 @@
+export const addr = ["직접입력","naver.com", "daum.net", "gmail.com", "hamail.net", "nate.com", "hotmail.com","paran.com", "yahoo.co.kr"]
+export const signUpPath = ["네이버검색", "우편물 수신", "자사직원 방문", "소개", "SNS", "광고"]
+export const specialty = ["화환전문점", "관업도매", "생화도매", "난도매", "꽃집"]
+
+
+
