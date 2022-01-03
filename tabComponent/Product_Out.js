6ea734bfb114dd0a1312a41adec1f79f65404541
sun <hama006@flroad.co.kr>
2022-01-03 17:48:56 +0900

    220103 '발주관리 & 리뷰관리 70%'

--- tabComponent/Product_Out.js
+++ tabComponent/Product_Out.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Alert, Image, Pressable, StyleSheet, View } from 'react-native';
-import { ScrollView, Text, Modal, SafeAreaView } from 'react-native';
+import { Alert, Image, Pressable, StyleSheet, View, ScrollView, Text, Modal, SafeAreaView } from 'react-native';
 import LocalSvg from 'react-native-svg/src/LocalSvg';
 import { D_Height, D_Width } from '../utils/deviceSize';
 import Arrow from '../img/arrow.svg';
@@ -10,13 +9,15 @@ import { FlatList } from 'react-native-gesture-handler';
 
 const Product_Out = ({ navigation }) => {
 
-    const [toggle, setToggle] = React.useState('ready');
+    const [toggle, setToggle] = React.useState('complete');
     const [isVisible, setIsVisible] = React.useState(false);
-    const [count, setCount] = React.useState([0,0,0,0,0]);
+    const [count, setCount] = React.useState([0, 0, 0, 0, 0]);
     const [list, setList] = React.useState([]);
     const [page, setPage] = React.useState(1);
+    const [test, setTest] = React.useState();
+    
 
-    const handleToggle = async(t) =>{
+    const handleToggle = async (t) => { //  상단 탭으로 바꿧을 경우 
         setList([]);
         setToggle(t);
         setPage(1);
@@ -25,37 +26,43 @@ const Product_Out = ({ navigation }) => {
     const _getData = () => {
         let url = `${Api.naviPush}${toggle}/?page=${page}&userid=flroad`;
         fetch(url)
-        .then((res)=>res.json())
-        .then((res)=>{
-            
-            let temp = [];
-            temp = list;
-            temp.push(...res.data);
-            console.log(res.data.length);
-            if(temp.length >= 10){
-                setPage(page + 1);
-            }
-            
-            setList(temp);
-        })
-        .catch(err=>console.log(err))
+            .then((res) => res.json())
+            .then((res) => {
+                if (list.length === 0 || ((page - 1) * 10) / list.length === 1) {
+                    setList([...list, ...res.data]);
+                    setPage(page + 1);
+                }
+            })
+            .catch(err => console.log(err))
     }
 
-    const _getCount = () =>{
+    const _getCount = () => {
         let url = `${Api.naviPushCount}?userid=flroad`
         fetch(url)
-        .then((res)=>res.json())
-        .then((res)=>setCount(res.data))
-        .catch(err=>console.log(err))
+            .then((res) => res.json())
+            .then((res) => setCount(res.data))
+            .catch(err => console.log(err))
     }
-    
-    React.useEffect(()=>{
+
+    React.useEffect(() => {
         _getCount();
         _getData();
+        // console.log(list);
     }, [toggle]);
 
-    const goDetail = (e) =>{
-        navigation.navigate('pushDetail', e);
+    const goNavi = (path, e) => {
+        navigation.navigate(path, e);
+    }
+
+    const _getImg = async (path, dealer) => {
+        let url = `${Api.naviPushImg}${path}/${dealer}`
+        await fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                setTest('https://flda.co.kr/data/flda/minishop/' + res.data[2] + "/" + res.data[0]);
+                setIsVisible(true)
+            })
+            .catch(err => console.log(err));
     }
 
     const RenderItem = ({ items }) => {
@@ -68,14 +75,14 @@ const Product_Out = ({ navigation }) => {
                         <Text style={styles.text_top}>{items.OrderNo}</Text>
                     </View>
 
-                    <Pressable onPress={() => toggle === 'ing' ? null : goDetail(items)} style={{ width: D_Width, flexDirection: 'column' }}>
+                    <Pressable onPress={() => toggle === 'ing' ? null : goNavi("pushDetail",items)} style={{ width: D_Width, flexDirection: 'column' }}>
                         <View style={{ marginHorizontal: 15, borderBottomWidth: 1, borderColor: 'grey' }}>
                             <Text style={styles.text_big}>배송일시 {items.rcvDate} {items.eventTime}{items.eventMinute}</Text>
                             <Text style={{ color: 'grey', fontWeight: 'bold', paddingTop: 10 }}>배송상세 {items.eventMemo}</Text>
                             <Text style={styles.text_small}>상품명 {items.GoodsTitle}</Text>
                             <Text style={styles.text_small}>배송지 {items.RcvAddr}</Text>
                             <Text style={styles.text_small}>수주화원 {items.DealerName}{"\n"}</Text>
-                            {toggle === 1 ?
+                            {toggle === 'ready' ?
                                 <View>
                                     <Pressable onPress={() => Alert.alert("알림", "주문을 취소할까요?", [{ text: "취소" }, { text: "확인", onPress: () => alert("주문 취소되었습니다.") }])} style={{ width: D_Width * 0.2, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>발주취소</Text></Pressable>
                                     <Text></Text>
@@ -96,19 +103,21 @@ const Product_Out = ({ navigation }) => {
 
                     <View style={{ width: D_Width, flexDirection: 'column' }}>
                         <View style={{ marginHorizontal: 15, borderBottomWidth: 1, borderColor: 'grey' }}>
-                    <Pressable onPress={() => goDetail(items)} style={{ width: D_Width, flexDirection: 'column' }}>
-                            <Text style={styles.text_big}>배송일시 {items.rcvDate} {items.eventTime}{items.eventMinute}</Text>
-                            {toggle === 'complete' ? <Text style={{ fontSize: 15, color: 'grey' }}>완료일시 {items.deliveryFinishDate}</Text> : null}
-                            <Text style={{ color: 'grey', fontWeight: 'bold', paddingTop: 10 }}>배송상세 {items.eventMemo}</Text>
-                            <Text style={styles.text_small}>상품명 {items.GoodsTitle}</Text>
-                            <Text style={styles.text_small}>배송지 {items.RcvAddr}</Text>
-                            <Text style={styles.text_small}>수주화원 {items.DealerName}</Text>
-                            <Text style={styles.text_small}>인수자 {items.RcvName}{"\n"}</Text>
+                            <Pressable onPress={() => goNavi("pushDetail",items)} style={{ width: D_Width, flexDirection: 'column' }}>
+                                <Text style={styles.text_big}>배송일시 {items.rcvDate} {items.eventTime}{items.eventMinute}</Text>
+                                {toggle === 'complete' ? <Text style={{ fontSize: 15, color: 'grey' }}>완료일시 {items.deliveryFinishDate}</Text> : null}
+                                <Text style={{ color: 'grey', fontWeight: 'bold', paddingTop: 10 }}>배송상세 {items.eventMemo}</Text>
+                                <Text style={styles.text_small}>상품명 {items.GoodsTitle}</Text>
+                                <Text style={styles.text_small}>배송지 {items.RcvAddr}</Text>
+                                <Text style={styles.text_small}>수주화원 {items.DealerName}</Text>
+                                <Text style={styles.text_small}>인수자 {items.RcvName}{"\n"}</Text>
                             </Pressable>
                             {toggle === 'complete' ?
                                 <View>
                                     <View style={{ flexDirection: 'row' }}>
-                                        <Pressable onPress={() => setIsVisible(true)} style={{ width: D_Width * 0.25, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>배송사진 보기</Text></Pressable>
+                                        <Pressable onPress={() => {
+                                            _getImg(items.Uid, items.DealerID);
+                                        }} style={{ width: D_Width * 0.25, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>배송사진 보기</Text></Pressable>
                                         <Text>{"  "}</Text>
                                         <Modal visible={isVisible} transparent>
                                             <SafeAreaView />
@@ -116,14 +125,14 @@ const Product_Out = ({ navigation }) => {
                                                 <Pressable onPress={() => setIsVisible(false)} style={{ marginHorizontal: 50, marginBottom: 20, flexDirection: 'row-reverse' }}>
                                                     <LocalSvg asset={XMark} width={25} height={25} fill={"#ffffff"} />
                                                 </Pressable>
-                                                <Image source={{ uri: 'https://source.unsplash.com/random' }} style={{ alignSelf: 'center', width: 250, height: 250 }}></Image>
+                                                <Image source={{ uri: test }} style={{ alignSelf: 'center', width: 250, height: 250 }}/>
                                             </View>
                                         </Modal>
                                         {/* <Pressable onPress={() => alert("배송사진 보기")} style={{ width: D_Width * 0.25, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>배송사진 보기</Text></Pressable> */}
                                         <Text>{"  "}</Text>
                                         <Pressable onPress={() => alert("구매완료")} style={{ width: D_Width * 0.2, height: D_Height * 0.06, backgroundColor: 'red', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>구매완료</Text></Pressable>
                                         <Text>{"  "}</Text>
-                                        <Pressable onPress={() => navigation.navigate("pushReview")} style={{ width: D_Width * 0.2, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>상품평쓰기</Text></Pressable>
+                                        <Pressable onPress={() => goNavi("pushReview",items.infouid) } style={{ width: D_Width * 0.2, height: D_Height * 0.06, backgroundColor: 'rgb(144,176,89)', justifyContent: 'center' }}><Text style={{ alignSelf: 'center', color: 'white' }}>상품평쓰기</Text></Pressable>
                                     </View>
                                     <Text></Text>
                                 </View>
@@ -172,10 +181,10 @@ const Product_Out = ({ navigation }) => {
                     </Text>
                     {toggle === 'waitnew' ?
                         <View style={styles.select_number_box}>
-                        <Text style={styles.select_number}>{count[1]}</Text>
-                    </View>
-                    :
-                    <Text style={styles.number}>{count[1]}</Text>
+                            <Text style={styles.select_number}>{count[1]}</Text>
+                        </View>
+                        :
+                        <Text style={styles.number}>{count[1]}</Text>
                     }
                 </Pressable>
                 <Pressable onPress={() => handleToggle('ing')} style={[styles.toggles, { borderColor: toggle === 'ing' ? 'rgb(144,176,89)' : "black" }]}>
@@ -184,10 +193,10 @@ const Product_Out = ({ navigation }) => {
                     </Text>
                     {toggle === 'ing' ?
                         <View style={styles.select_number_box}>
-                        <Text style={styles.select_number}>{count[2]}</Text>
-                    </View>
-                    :
-                    <Text style={styles.number}>{count[2]}</Text>
+                            <Text style={styles.select_number}>{count[2]}</Text>
+                        </View>
+                        :
+                        <Text style={styles.number}>{count[2]}</Text>
                     }
                 </Pressable>
                 <Pressable onPress={() => handleToggle('complete')} style={[styles.toggles, { borderColor: toggle === 'complete' ? 'rgb(144,176,89)' : "black" }]}>
@@ -196,10 +205,10 @@ const Product_Out = ({ navigation }) => {
                     </Text>
                     {toggle === 'complete' ?
                         <View style={styles.select_number_box}>
-                        <Text style={styles.select_number}>{count[3]}</Text>
-                    </View>
-                    :
-                    <Text style={styles.number}>{count[3]}</Text>
+                            <Text style={styles.select_number}>{count[3]}</Text>
+                        </View>
+                        :
+                        <Text style={styles.number}>{count[3]}</Text>
                     }
                 </Pressable>
                 <Pressable onPress={() => handleToggle('cancel')} style={[styles.toggles, { borderColor: toggle === 'cancel' ? 'rgb(144,176,89)' : "black" }]}>
@@ -208,10 +217,10 @@ const Product_Out = ({ navigation }) => {
                     </Text>
                     {toggle === 'cancel' ?
                         <View style={styles.select_number_box}>
-                        <Text style={styles.select_number}>{count[4]}</Text>
-                    </View>
-                    :
-                    <Text style={styles.number}>{count[4]}</Text>
+                            <Text style={styles.select_number}>{count[4]}</Text>
+                        </View>
+                        :
+                        <Text style={styles.number}>{count[4]}</Text>
                     }
                 </Pressable>
             </View>
@@ -246,10 +255,9 @@ const Product_Out = ({ navigation }) => {
         <FlatList
             data={list}         //  컴포넌트에 들어갈 데이터
             keyExtractor={(item, index) => index}
-            renderItem={( {item} ) => <RenderItem items={item} />}   // 반복시켜 보여줄 컴포넌트
-            onEndReached={()=>{
+            renderItem={({ item }) => <RenderItem items={item} />}   // 반복시켜 보여줄 컴포넌트
+            onEndReached={() => {
                 _getData();
-                // console.log(page);
             }} //  스크롤의 마지막에 닿았을 때 이벤트
         />
     </>
@@ -281,25 +289,25 @@ const styles = StyleSheet.create({
         color: 'grey',
         fontSize: 12
     },
-    text_small:{
+    text_small: {
         color: 'grey',
         fontWeight: 'bold',
-        paddingTop: 1 
+        paddingTop: 1
     },
-    text_big:{
+    text_big: {
         fontSize: 18,
         color: 'grey',
         fontWeight: 'bold'
     },
-    text_top:{
+    text_top: {
         alignSelf: 'center',
         fontSize: 12,
         color: 'grey',
-        paddingVertical:10
+        paddingVertical: 10
     },
-    text_none_data:{
-        fontSize:16,
-        alignSelf:'center',
-        marginVertical:10
+    text_none_data: {
+        fontSize: 16,
+        alignSelf: 'center',
+        marginVertical: 10
     }
 })
--- tabComponent/Product_Out_Insert_Review.js
+++ tabComponent/Product_Out_Insert_Review.js
@@ -1,17 +1,67 @@
+import { useRoute } from '@react-navigation/native';
 import * as React from 'react';
 import { ScrollView, Text, View, Dimensions, Pressable, FlatList, TextInput } from 'react-native';
 import { Rating } from 'react-native-ratings';
 import LocalSvg from 'react-native-svg/src/LocalSvg';
 import Arrow from '../img/arrow.svg';
 import { D_Height, D_Width } from '../utils/deviceSize';
+import Api from '../config.json';
+import axios from 'axios';
 
 const Product_Out_Insert_Review = ({ navigation }) => {
 
-    const [review, setReview] = React.useState();
+    const [review, setReview] = React.useState(null);
     const [starRating, setStarRating] = React.useState(0);
 
-    return <>
+    const route = useRoute();
+    let ruid = route.params
+    
+    React.useEffect(()=>{
+        const _getData = async () => {
+            let url = `${Api.naviReview}${ruid}`
+            fetch(url)
+            .then(res=>res.json())
+            .then(res=>{
+                if(res === null){
 
+                }
+                else{
+                    setReview(res.content);
+                    setStarRating(res.ParPoint);
+                }
+            })
+            .catch(err=>console.log(err))
+        }
+        _getData();
+    },[]);
+    
+    const handlReview = async () =>{
+        try{
+            const result = await axios({
+                method:'post',
+                url:"http://localhost:5000/sun/sun/order/review/insert",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                data:{
+                    'infouid':ruid,
+                    'content':review,
+                    'userid':'flroad',  // 수정필요 전역관리
+                    'username':'플로드',    //수정필요 전역관리 
+                    'parPoint':starRating
+                }
+            })
+
+        }catch(err){
+            console.log(err);
+        }
+        
+    }
+    
+
+    return <>
+        
         <View style={{ width: D_Width, height: (D_Height * 0.08), backgroundColor: "black", flexDirection: "row", justifyContent: "center" }}>
             <View style={{ width: (D_Width * 0.95), height: '100%', flexDirection: 'row' }}>
                 <Pressable onPress={() => navigation.goBack()} style={{ flex: 1, transform: [{ rotate: '180deg' }], justifyContent: 'center' }}>
@@ -46,7 +96,7 @@ const Product_Out_Insert_Review = ({ navigation }) => {
                 style={{alignSelf:'center', paddingHorizontal:10}}
                 type='star'
                 ratingCount={5}
-                startingValue={0}
+                startingValue={starRating}
                 imageSize={16}
                 tintColor='rgb(242,242,242)'
                 onFinishRating={setStarRating}
@@ -57,19 +107,23 @@ const Product_Out_Insert_Review = ({ navigation }) => {
         </View>
         <View style={{ flexDirection: 'row', marginHorizontal: 10, height: D_Height * 0.05 }}>
             <TextInput style={{ flex: 5, borderWidth: 1, borderColor: "grey", paddingHorizontal: 8 }}
-                placeholder={'최대 한글 200자까지 가능합니다.'}
+                placeholder={review ? review : '최대 한글 200자까지 가능합니다.'}
                 onChangeText={setReview}
             />
             {/* <Pressable style={{ flex: 1, backgroundColor: 'rgb(144,176,89)', justifyContent:'center' }}><Text style={{alignSelf:'center'}}>등록</Text></Pressable> */}
         </View>
         <Text/>
-        <Pressable onPress={()=>alert("파일첨부")} style={{marginHorizontal:10, justifyContent:'center',width:D_Width*0.2, backgroundColor:'rgb(144,176,89)', height:D_Height*0.05}}><Text style={{alignSelf:'center', color:'white'}}>파일첨부</Text></Pressable>
+        <Pressable onPress={()=>alert("파일첨부 expo <-> cli 모듈 다름으로 미구현")} style={{marginHorizontal:10, justifyContent:'center',width:D_Width*0.2, backgroundColor:'rgb(144,176,89)', height:D_Height*0.05}}><Text style={{alignSelf:'center', color:'white'}}>파일첨부</Text></Pressable>
         <Text/>
 
-        <Pressable onPress={()=>{
+        <Pressable onPress={() => {
             alert('등록완료')
-            navigation.goBack();
-        }} style={{ marginHorizontal:10, justifyContent:'center', backgroundColor:'rgb(144,176,89)', height:D_Height*0.05}}><Text style={{alignSelf:'center', color:'white'}}>등록</Text></Pressable>
+            handlReview();
+            // navigation.goBack();
+        }} style={{ marginHorizontal: 10, justifyContent: 'center', backgroundColor: 'rgb(144,176,89)', height: D_Height * 0.05 }}><Text style={{ alignSelf: 'center', color: 'white' }}>
+                {review === null ? "등록" : "수정" }
+            </Text>
+        </Pressable>
     </>
 }
 export default Product_Out_Insert_Review
